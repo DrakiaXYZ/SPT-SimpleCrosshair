22d009a8fb6819ab1781c284616938e73eadac7e
DrakiaXYZ <565558+TheDgtl@users.noreply.github.com>
2025-03-16 23:13:30 -0700

    Bump version for SPT 3.11 Clean up a warning from BepInEx

--- Patches/BattleUIScreenShowPatch.cs
+++ Patches/BattleUIScreenShowPatch.cs
@@ -11,18 +11,22 @@ namespace SimpleCrosshair.Patches
     {
         protected override MethodBase GetTargetMethod()
         {
-            return AccessTools.Method(typeof(EftBattleUIScreen),
+            return AccessTools.Method(typeof(EftBattleUIScreen).BaseType,
                                       nameof(EftBattleUIScreen.Show),
                                       new Type[] { typeof(GamePlayerOwner) });
         }
 
         [PatchPostfix]
-        public static void PatchPostfix(EftBattleUIScreen __instance)
+        public static void PatchPostfix(object __instance)
         {
             // Don't attach if we're in the hideout, because OnRegisterPlayer isn't called in the hideout so other things don't work right
             if (!GameUtils.IsGameActive) return;
 
-            Plugin.Instance.TryAttachToBattleUIScreen(__instance);
+            EftBattleUIScreen screen = __instance as EftBattleUIScreen;
+            if (screen != null)
+            {
+                Plugin.Instance.TryAttachToBattleUIScreen(screen);
+            }
         }
     }
 }
